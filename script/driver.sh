037f632434ebec0dcd089f2c6c5b7a107df3dea5
kkunapuli <kgarot@gmail.com>
2019-07-10 14:06:28 -0500

    basic driver; tested individual components, not java execution

--- script/driver.sh
+++ script/driver.sh
@@ -1,17 +1,44 @@
 #!/bin/bash
 
+#driver.sh [filename] [reducer port]
+
 #expect first argument to be filename to process
 filename=$1
+#compute total number of lines
+num_lines=`cat $filename | wc -l`
+#assume local host
+redHost="127.0.0.1"
+#reducer port number - expected as second argument
+redPort=$2
 
-cmdSplit1="java -classpath <classpath splitter> <arg1> <arg2>"
-cmdStem=""
-cmdMap=""
-cmdSend=""
+#assume system is being run from project directory
+cpath="bin/"
 
-#process file by splitting into 4 parts
-$(cmdSplit1) | $(cmdStem) | $(cmdMap) | $(cmdSend) &
-pid1=$!
+cmdSplit="java -classpath $cpath Splitter $filename $num_lines"
+cmdStem="java -classpath $cpath Stemmer"
+cmdMap="java -classpath $cpath Mapper"
+cmdSend="java -classpath $cpath Sender $redHost $redPort"
 
-wait $pid1
+#keep array of pids
+pid_arr=(0 0 0 0)
 
+#start al processes in background
+for i in 1 2 3 4; do
+    #make the command for splitter i
+	fullCmdSplit="$cmdSplit $i"
+	
+	#start all processes
+	($fullCmdSplit | $cmdStem | $cmdMap | $cmdSend) &
+	pid=$!
+	
+	#store pid
+	idx=`echo "$i - 1" | bc -l`
+	pid_arr[$idx]=$pid
+	
+done
+
+#wait on all pids
+for p in "${pid_arr[@]}"; do
+	wait $p
+done
 
