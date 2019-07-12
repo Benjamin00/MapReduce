6b2d749bf16a95bba53772ad4c11c1cce44d27a4
mikeyesudas12345 <mikeyesudas@gmail.com>
2019-07-12 14:51:56 -0500

    Basic Input

--- src/Splitter.java
+++ src/Splitter.java
@@ -1,9 +1,25 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
 
 public class Splitter {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+
+		// Takes in input from the command line
+
+		Reader readerObject = new InputStreamReader(System.in);
+		BufferedReader bufferedReaderObj = new BufferedReader(readerObject);
+		bufferedReaderObj.readLine(); 
+
+
+
+
+
 	}
+
+
 
 }
