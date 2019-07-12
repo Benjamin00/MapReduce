cabfeaa10a8aef6f2619659949dd962f43bc2e41
Vivi Song <vivisong1903@gmail.com>
2019-07-12 15:33:22 -0500

    Read Inputs Mapper and Stemmer

--- src/Mapper.java
+++ src/Mapper.java
@@ -1,11 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Mapper {
 	static List<String> stringList = new ArrayList<String>();
-	public static void main(String[] args) {
-		Mapper("desk is big desk");
-		System.out.print(stringList);
+	public static void main(String[] args) throws IOException {
+		Reader readerObject = new InputStreamReader(System.in);
+		BufferedReader bufferedReaderObj = new BufferedReader(readerObject);
+		String str = bufferedReaderObj.readLine(); 
+        Mapper(str);
+        System.out.print(stringList);
 	}
 	
 	
--- src/Stemmer.java
+++ src/Stemmer.java
@@ -1,9 +1,15 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
 
 public class Stemmer {
 
-	public static void main(String[] args) {
-		Stemmer("This is an example");
-
+	public static void main(String[] args) throws IOException {
+		Reader readerObject = new InputStreamReader(System.in);
+		BufferedReader bufferedReaderObj = new BufferedReader(readerObject);
+		String str = bufferedReaderObj.readLine(); 
+        Stemmer(str);
 	}
 	
 	public static void Stemmer(String line)
