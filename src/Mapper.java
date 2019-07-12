fc2be8015614f4761e8fefa56fd1520dd91fde78
Vivi Song <vivisong1903@gmail.com>
2019-07-12 14:57:36 -0500

    Mapper Class

--- src/Mapper.java
+++ src/Mapper.java
@@ -1,9 +1,11 @@
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class Mapper {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		static Map<String, List<Pair>> map=new HashMap<String, List<Pair>>(); 
 	}
 
 }
