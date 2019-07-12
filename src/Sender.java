3ac491a3a31ceec00f9ecd2bed8fa42c53399c20
benjamin00 <[benjaminhill00@gmail.com]>
2019-07-12 14:48:00 -0500

    Added IP sending functionality to sender, can call startconnection,sendmessage,and stopconnection

--- src/Sender.java
+++ src/Sender.java
@@ -1,9 +1,46 @@
+import java.net.*;
+import java.util.*;
+import java.io.*;
 
 public class Sender {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+	private Socket clientSocket;
+    private PrintWriter out;
+    private BufferedReader in;
+    
+		//function to begin connection to a given ip and port
+		public void startConnection(String ip, int port) {
+		       try {
+		    	clientSocket = new Socket(ip, port);
+		        out = new PrintWriter(clientSocket.getOutputStream(), true);
+		        in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+		       }catch(Exception e) {
+		    	   System.out.println("Error Starting Connection");
+		    	   e.printStackTrace();
+		       }
+		    }
+		
+		//send a message through the open connection
+		    public String sendMessage(String msg) {
+		        try {
+					out.println(msg);
+					String resp = in.readLine();
+					return resp;
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+		        return "ERROR";
+		    }
+		    
+		 //close the connection
+		    public void stopConnection() {
+		        try {
+					in.close();
+					out.close();
+			        clientSocket.close();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+		    }
 }
