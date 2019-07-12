8756337fcfc84fa3d9b844eeb417f89ed88bac2c
mikeyesudas12345 <mikeyesudas@gmail.com>
2019-07-12 17:19:30 -0500

    Further Changes

--- src/Splitter.java
+++ src/Splitter.java
@@ -26,6 +26,61 @@ public class Splitter {
 			numLinesInEachQuarter[y] = numLinesInEachQuarter[y] + 1;
 		}
 
+		// Determines the line numbers
+		
+		int startingLineNumbers[] = new int[4];
+
+		startingLineNumbers[0] = 0;
+		startingLineNumbers[1] = numLinesInEachQuarter[0];
+		startingLineNumbers[2] = numLinesInEachQuarter[0] 
+								 + numLinesInEachQuarter[1];
+		startingLineNumbers[3] = numLinesInEachQuarter[0] 
+								 + numLinesInEachQuarter[1] 
+								 + numLinesInEachQuarter[2];
+
+
+		// Reads from the file according to the line numbers in the array
+
+		// TODO: We need an example text file so that we can test this out. 
+		// TODO: This is only one potential way to read from a file. It could be fixed later.  
+
+		int numIterations = 0; 
+
+		int beginningOfQuarter;
+		int endOfQuarter; 
+
+		if (quarterNumber == 1)
+		{
+			beginningOfQuarter = startingLineNumbers[0];
+			endOfQuarter = startingLineNumbers[1];
+		}
+		else if (quarterNumber == 2)
+		{
+			beginningOfQuarter = startingLineNumbers[1] + 1;
+			endOfQuarter = startingLineNumbers[2];
+		}
+		else if (quarterNumber == 3)
+		{
+			beginningOfQuarter = startingLineNumbers[2] + 1;
+			endOfQuarter = startingLineNumbers[3];
+		}
+		else if (quarterNumber == 4)
+		{
+			beginningOfQuarter = startingLineNumbers[3] + 1;
+			endOfQuarter = numLines;
+		}
+		else
+		{
+			// Do Nothing
+		}
+
+
+
+
+
+
+
+
 
 		
 		
@@ -36,7 +91,19 @@ public class Splitter {
 		System.out.println(numLines);
 		System.out.println(quarterNumber);
 
+		System.out.println("\n" + "*** NumLinesInEachQuarter ***");
+
+		for (int z = 0; z < numLinesInEachQuarter.length; z++)
+		{
+			System.out.println(numLinesInEachQuarter[z]);
+		}
+
+		System.out.println("\n" + "*** StartingLineNumbersForEachQuarter ***" + "\n");
 		
+		for (int a = 0; a < startingLineNumbers.length; a++)
+		{
+			System.out.println(startingLineNumbers[a]);
+		}
 
 
 
