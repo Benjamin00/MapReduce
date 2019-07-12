4fbeb72717888d7786df25f6727b5fb8143bc304
kkunapuli <kgarot@gmail.com>
2019-07-12 15:36:41 -0500

    resolve merge conflict

--- src/Mapper.java
+++ src/Mapper.java
@@ -6,20 +6,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Mapper {
-<<<<<<< HEAD
-	static List<String> stringList = new ArrayList<String>();
+	public List<String> stringList = new ArrayList<String>();
 	public static void main(String[] args) throws IOException {
 		Reader readerObject = new InputStreamReader(System.in);
 		BufferedReader bufferedReaderObj = new BufferedReader(readerObject);
 		String str = bufferedReaderObj.readLine(); 
-        Mapper(str);
-        System.out.print(stringList);
-=======
-	public List<String> stringList = new ArrayList<String>();
-	public static void main(String[] args) {
-		Mapper myMap = new Mapper("desk is big desk");
-		System.out.print(myMap.stringList);
->>>>>>> 3ee26a0156544b313aec6e9f166681b92c74a1eb
+        Mapper myMap = new Mapper(str);
+        System.out.print(myMap.stringList);
 	}
 
 
