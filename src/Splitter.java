10dfae235322c451a5b286b616dd6ad3f56f8477
mikeyesudas12345 <mikeyesudas@gmail.com>
2019-07-19 13:04:44 -0500

    Still working on it

--- src/Splitter.java
+++ src/Splitter.java
@@ -2,7 +2,7 @@ import java.io.*;
 import java.util.*;
 //import java.lang.Integer;
 
-import javax.lang.model.util.ElementScanner6;
+//import javax.lang.model.util.ElementScanner6;
 
 public class Splitter {
 
@@ -12,91 +12,46 @@ public class Splitter {
 		int numLines = Integer.parseInt(args[1]);
 		int quarterNumber = Integer.parseInt(args[2]);
 
-		// Calculates the line numbers for each of the quarters 
-
-		/*
-
-		int basicQuarterNumber = numLines / 4; 
-		int numTimesToAddOne = numLines % 4;
-		int[] numLinesInEachQuarter = new int[4];
-
-		for (int x = 0; x < numLinesInEachQuarter.length; x++)
-		{
-			numLinesInEachQuarter[x] = basicQuarterNumber;
-		}
-
-		for (int y = 0; y < numTimesToAddOne; y++)
-		{
-			numLinesInEachQuarter[y] = numLinesInEachQuarter[y] + 1;
-		}
-
-		*/
-
 		// Determines the line numbers
+
+		int startingLineNumber = 0;
+		int endingLineNumber = 0;
 		
 		if (quarterNumber == 1)
 		{
-
+			startingLineNumber = 1;
+			endingLineNumber = (int) (numLines / 4);
 		}
 		else if(quarterNumber == 2)
 		{
-
+			startingLineNumber = (int) (numLines / 4);
+			endingLineNumber = (int) ((numLines * 2) / 4);
 		}
 		else if(quarterNumber == 3)
 		{
-
+			startingLineNumber = (int) ((numLines * 2) / 4);
+			endingLineNumber = (int) ((numLines * 3) / 4);
 		}
 		else if(quarterNumber == 4)
 		{
-
+			startingLineNumber = (int) ((numLines * 3) / 4);
+			endingLineNumber = numLines;
 		}
 		else
 		{
-			// Do nothing 
-			// System.out.println("Invalid Quarter Number");
+			System.out.println("Invalid Quarter Number");
 		}
 
 
 		// Reads from the file according to the line numbers in the array
 
-		// TODO: We need an example text file so that we can test this out. 
-		// TODO: This is only one potential way to read from a file. It could be fixed later.  
-
-		int numIterations = 0; 
-
-		int beginningOfQuarter;
-		int endOfQuarter; 
-
-		if (quarterNumber == 1)
-		{
-			beginningOfQuarter = startingLineNumbers[0];
-			endOfQuarter = startingLineNumbers[1];
-		}
-		else if (quarterNumber == 2)
-		{
-			beginningOfQuarter = startingLineNumbers[1] + 1;
-			endOfQuarter = startingLineNumbers[2];
-		}
-		else if (quarterNumber == 3)
-		{
-			beginningOfQuarter = startingLineNumbers[2] + 1;
-			endOfQuarter = startingLineNumbers[3];
-		}
-		else if (quarterNumber == 4)
-		{
-			beginningOfQuarter = startingLineNumbers[3] + 1;
-			endOfQuarter = numLines;
-		}
-		else
-		{
-			// Do Nothing
-		}
-
-		// *** File is opened and read from here 
+		// File is opened and read from here 
 
 		// TODO: We need to make a test case file.
 
-		File textFile = new File(/* Name of example text file should go here */);
+		/*
+
+		File textFile = new File();
 		
 		BufferedReader buffReaderObj = new BufferedReader(new FileReader(textFile));
 
@@ -117,9 +72,11 @@ public class Splitter {
 					// contentsOfQuarter += "\n";
 			}
 		}
+		
 
 
 		System.out.println(contentsOfQuarter);
+		*/
 
 
 
@@ -145,26 +102,16 @@ public class Splitter {
 		
 		
 		// TESTING PURPOSES
+
+		System.out.println("Starting Line Number: ");
+
+		System.out.println(startingLineNumber + "\n");
+
+		System.out.println("Ending Line Number: ");
+
+		System.out.println(endingLineNumber + "\n");
+
 		
-		System.out.println(filename);
-		System.out.println(numLines);
-		System.out.println(quarterNumber);
-
-		System.out.println("\n" + "*** NumLinesInEachQuarter ***");
-
-		for (int z = 0; z < numLinesInEachQuarter.length; z++)
-		{
-			System.out.println(numLinesInEachQuarter[z]);
-		}
-
-		System.out.println("\n" + "*** StartingLineNumbersForEachQuarter ***" + "\n");
-		
-		for (int a = 0; a < startingLineNumbers.length; a++)
-		{
-			System.out.println(startingLineNumbers[a]);
-		}
-
-
 
 
 
